3d719282d6d07d2bb672bb595bc5be158bbb7e35
PizzaPower55 <justenmao@gmail.com>
2024-06-26 15:12:59 -0500

    fully connected layer based model implemented

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
 import sys
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import time, tqdm
 
 class model(nn.Module):
-    def __init__(self, lr=0.0001, lrDecay=0.95, **kwargs):
+    def __init__(self, lr=0.0001, lrDecay=0.95, device='gpu', **kwargs):
         super(model, self).__init__()
 
         self.visualModel = None
@@ -11,28 +13,120 @@ class model(nn.Module):
         self.fusionModel = None
         self.fcModel = None
 
+        self.device = ("cuda" if torch.cuda.is_available() else "cpu")
+
         self.createVisualModel()
         self.createAudioModel()
         self.createFusionModel()
         self.createFCModel()
-
+        
+        self.visualModel = self.visualModel.to(self.device)
+        self.audioModel = self.audioModel.to(self.device)
+        self.fcModel = self.fcModel.to(self.device)
+        
+        self.optim = torch.optim.Adam(self.parameters(), lr = lr)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optim, step_size = 1, gamma=lrDecay)
+        
+        self.loss_fn = nn.CrossEntropyLoss()
+        
     def createVisualModel(self):
-        pass
+        self.visualModel = nn.Sequential(nn.Flatten(), nn.Linear(112*112, 512), nn.ReLU(), nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 128))
 
     def createAudioModel(self):
-        pass
+        self.audioModel = nn.Sequential(nn.Flatten(), nn.Linear(299*13, 512), nn.ReLU(), nn.Linear(512, 256), nn.ReLU(), nn.Linear(256, 128))
+
 
     def createFusionModel(self):
         pass
 
     def createFCModel(self):
-        pass
+        self.fcModel = nn.Sequential(nn.Linear(256, 128), nn.ReLU(), nn.Linear(128,64), nn.ReLU(), nn.Linear(64, 2))
     
     def train_network(self, loader, epoch, **kwargs):
-        pass
+        
+        self.train()
+        self.scheduler.step(epoch-1)
+        lr = self.optim.param_groups[0]['lr']
+        index, top1, loss = 0, 0, 0
+        for num, (audioFeatures, visualFeatures, labels) in enumerate(loader, start=1):
+                self.zero_grad()
 
+                # print('audioFeatures shape: ', audioFeatures.shape)
+                # print('visualFeatures shape: ', visualFeatures.shape)
+                # print('labels shape: ', labels.shape)
+                
+                # print('audio feature: ', audioFeatures)
+                # print('visual features: ', visualFeatures)
+
+                audioFeatures = torch.unsqueeze(audioFeatures, dim=1)  
+                # print('audioFeatures after unsqueeze: ', audioFeatures.shape)            
+                
+                audioFeatures = audioFeatures.to(self.device)
+                visualFeatures = visualFeatures.to(self.device)
+                labels = labels.squeeze().to(self.device)
+                                
+                audioEmbed = self.audioModel(audioFeatures)
+                # print('audio embed shape: ', audioEmbed.shape)
+                visualEmbed = self.visualModel(visualFeatures)
+                # print('visual embed shape: ', visualEmbed.shape)
+                
+                avfusion = torch.cat((audioEmbed, visualEmbed), dim=1)
+                # print('avfusion shape: ', avfusion.shape)
+                
+                fcOutput = self.fcModel(avfusion)
+                # print('fc output shape: ', fcOutput.shape)
+                
+                nloss = self.loss_fn(fcOutput, labels)
+                
+                self.optim.zero_grad()
+                nloss.backward()
+                self.optim.step()
+                
+                loss += nloss.detach().cpu().numpy()
+                
+                top1 += (fcOutput.argmax(1) == labels).type(torch.float).sum().item()
+                index += len(labels)
+                sys.stderr.write(time.strftime("%m-%d %H:%M:%S") + \
+                " [%2d] Lr: %5f, Training: %.2f%%, "    %(epoch, lr, 100 * (num / loader.__len__())) + \
+                " Loss: %.5f, ACC: %2.2f%% \r"        %(loss/(num), 100 * (top1/index)))
+                sys.stderr.flush()  
+        sys.stdout.write("\n")
+        
+        return loss/num, lr
+        
     def evaluate_network(self, loader, **kwargs):
-        pass
+        self.eval()
+        predScores = []
+        
+        loss, top1, index, numBatches = 0, 0, 0, 0
+        
+        for audioFeatures, visualFeatures, labels in tqdm.tqdm(loader):
+            
+            audioFeatures = torch.unsqueeze(audioFeatures, dim=1)
+            audioFeatures = audioFeatures.to(self.device)
+            visualFeatures = visualFeatures.to(self.device)
+            labels = labels.squeeze().to(self.device)
+            
+            with torch.no_grad():
+                
+                audioEmbed = self.audioModel(audioFeatures)
+                visualEmbed = self.visualModel(visualFeatures)
+                
+                avfusion = torch.cat((audioEmbed, visualEmbed), dim=1)
+                
+                fcOutput = self.fcModel(avfusion)
+                
+                nloss = self.loss_fn(fcOutput, labels)
+                
+                loss += nloss.detach().cpu().numpy()
+                top1 += (fcOutput.argmax(1) == labels).type(torch.float).sum().item()
+                index += len(labels)
+                numBatches += 1
+                
+        print('eval loss ', loss/numBatches)
+        print('eval accuracy ', top1/index)
+        
+        return top1/index
 
     def saveParameters(self, path):
         torch.save(self.state_dict(), path)
